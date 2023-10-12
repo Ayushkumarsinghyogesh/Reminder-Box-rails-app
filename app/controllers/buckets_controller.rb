fbcd9f5e0d5315875c6ffe85bf8b81d62be074b1
Ayushkumarsinghyogesh <ayushkumarsinghyogesh@gmail.com>
2023-10-12 17:09:01 +0530

    commit

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,7 +1,33 @@
 class BucketsController < ApplicationController
 
+    def index
+        @empty_buckets = Bucket.get_by_status("Empty")
+        @pending_buckets = Bucket.get_by_status("Pending")
+        @completed_buckets = Bucket.get_by_status("Completed")
+    end
+
+
     def new
         @bucket = Bucket.new
     end 
+    
+
+    def create
+        @bucket = Bucket.create(bucket_params(:name, :description))
+        @bucket.status = "Empty"
+        @bucket.save
+        redirect_to bucket_path(@bucket)
+    end 
+
+    def show
+        @bucket = Bucket.find(params[:id])
+    end
+
+
+
+    private
+    def bucket_params(*args)
+        params.require(:bucket).permit(*args)
+    end
     
 end
--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,3 +1,12 @@
 class Bucket < ApplicationRecord
     has_many :tasks
+
+
+    def self.get_by_status(status)
+        self.all.select do |bucket|
+            bucket.status == status
+        end
+    end 
+
+    
 end
