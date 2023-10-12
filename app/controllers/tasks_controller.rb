8d1a4210ee0d5397f77af76cda9a18cce783ec1b
Ayushkumarsinghyogesh <ayushkumarsinghyogesh@gmail.com>
2023-10-12 18:29:19 +0530

    New commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+
+    def new
+        @bucket = Bucket.new
+    end
+
+    def create
+        @bucket = Bucket.create(bucket_params(:name, :description))
+        @bucket.status = "Empty"
+        @bucket.save
+        redirect_to bucket_path(@bucket)
+    end
+
+    
 end
