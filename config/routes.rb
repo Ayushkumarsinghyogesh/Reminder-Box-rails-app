68b091447c57ab1b6d5d4167a3265992fafb8fb4
Ayushkumarsinghyogesh <ayushkumarsinghyogesh@gmail.com>
2023-10-13 18:26:02 +0530

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static/welcome'
+  # get 'static/welcome'
   resources :tasks
   resources :buckets
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
  
   # Defines the root path route ("/")
   root "static#welcome"
+
 end
